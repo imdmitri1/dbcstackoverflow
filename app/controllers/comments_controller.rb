6f6443e99a9ab9a59b4fb5109d1af9cabba82d93
Sam Tietze <samtietze@gmail.com>
2017-09-07 18:08:55 -0500

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
-get '/comments/new' do
+get '/comments/:commentable_type/:commentable_id/new' do
   erb :'comments/new'
 end
 
-get '/comments/:id/edit' do
+get '/comments/:commentable_type/:commentable_id/edit' do
   @comment = Comment.find_by(id: params[:id])
   erb :'comments/edit'
 end
 
-post '/comments/:commentable_type/commentable_id' do
+post '/comments/:commentable_type/:commentable_id' do
   @comment = Comment.new(commentable_type: params[:commentable_type:], commentable_id: params[:commentable_id], comment_body: params[:comment_body], commenter_id: current_user.id)
   if @comment.save
     # need to redirect to the show question page!
@@ -18,7 +18,7 @@ post '/comments/:commentable_type/commentable_id' do
   end
 end
 
-put '/comments/:commentable_type/commentable_id' do
+put '/comments/:commentable_type/:commentable_id' do
   @comment = Comment.find_by(commentable_type: params[:commentable_type:], commentable_id: params[:commentable_id], commenter_id: current_user.id)
   @comment.update_attributes(comment_body: params[:comment_body])
   if @comment.save

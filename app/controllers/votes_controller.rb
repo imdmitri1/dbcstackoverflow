a72995a5ca7df98c61412fdeb66a5d7d144caee9
Dmitri Stefanisin <email.ds@protonmail.com>
2017-09-08 12:11:05 -0500

    add some small changes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,14 @@
-post '/upvotes/:votable_type/:votable_id' do
+get '/upvotes/:votable_type/:votable_id' do
   @vote = Vote.find_by(votable_type: params[:votable_type], votable_id: params[:votable_id], voter_id: current_user.id)
   if @vote == nil
-    @vote = Vote.create(votable_type: params[:votable_type], votable_id: params[:votable_id], voter_id: current_user.id, value: 1)
+    @vote = Vote.create(votable_type: params[:votable_type].to_s, votable_id: params[:votable_id], voter_id: current_user.id, value: 1)
   else
     @vote.update_attributes(value: 1)
   end
   redirect back
 end
 
-post '/downvotes/:votable_type/:votable_id' do
+get '/downvotes/:votable_type/:votable_id' do
   @vote = Vote.find_by(votable_type: params[:votable_type], votable_id: params[:votable_id], voter_id: current_user.id)
   if @vote == nil
     @vote = Vote.create(votable_type: params[:votable_type], votable_id: params[:votable_id], voter_id: current_user.id, value: -1)
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,7 +5,7 @@ class Vote < ActiveRecord::Base
   validates_presence_of :value, :voter_id
 
   def self.total_value
-    # self.all.map(&:value).reduce("+")
-    self.all.sum(:value)
+    self.all.map(&:value).reduce(:+)
+    # self.all.sum(:value)
   end
 end

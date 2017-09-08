14926bac8695508f1b2dabd6311ff0537d6cad98
Dmitri Stefanisin & Sam Tietze <pair+imdmitri1.samtietze@devbootcamp.com>
2017-09-08 17:11:00 -0500

    done with db

--- db/migrate/20170907143818_create_questions.rb
+++ db/migrate/20170907143818_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
     create_table :questions do |t|
-      t.string :title, { null: false, limit: 256 }
+      t.string :title, { null: false, limit: 256, unique: true }
       t.string :question_body, { null: false }
       t.integer :author_id
       t.integer :views, default: 0
--- db/migrate/20170908152756_create_tags.rb
+++ db/migrate/20170908152756_create_tags.rb
@@ -1,7 +1,7 @@
 class CreateTags < ActiveRecord::Migration[5.1]
   def change
     create_table :tags do |t|
-      t.string :name, { null: false, limit: 256 }
+      t.string :name, { null: false, limit: 256, unique: true }
 
       t.timestamps
     end

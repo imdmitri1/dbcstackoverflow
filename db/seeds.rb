f932536570c6ab0b7e5d0e2830d39b459b903357
Dmitri Stefanisin <email.ds@protonmail.com>
2017-09-09 12:07:24 -0500

    change id numbers in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,25 +15,25 @@ User.create(name: "dbc", email: "dbc@none.none", password: "password", user_phot
 end
 
 31.times do
-  Question.create!(title: Faker::Hacker.say_something_smart, question_body: Faker::Hipster.paragraph(rand(4..15)), author_id: rand(1..31))
+  Question.create!(title: Faker::Hacker.say_something_smart, question_body: Faker::Hipster.paragraph(rand(4..15)), author_id: rand(234..264))
 end
 
 90.times do
-  Answer.create(answer_body: Faker::Hipster.paragraph(rand(4..10)), question_id: rand(1..31), answerer_id: rand(1..31))
+  Answer.create(answer_body: Faker::Hipster.paragraph(rand(4..10)), question_id: rand(183..213), answerer_id: rand(234..264))
 end
 
 10.times do
-  Comment.create(commentable_type: "Question", commentable_id: rand(1..31), comment_body: Faker::Hacker.say_something_smart, commenter_id: rand(1..31))
+  Comment.create(commentable_type: "Question", commentable_id: rand(183..213), comment_body: Faker::Hacker.say_something_smart, commenter_id: rand(234..264))
 end
 
 100.times do
-  Comment.create(commentable_type: "Answer", commentable_id: rand(1..90), comment_body: Faker::Hacker.say_something_smart, commenter_id: rand(1..31))
+  Comment.create(commentable_type: "Answer", commentable_id: rand(591..680), comment_body: Faker::Hacker.say_something_smart, commenter_id: rand(234..264))
 end
 
 300.times do
-  Vote.create(votable_type: "Question", votable_id: rand(1..31), voter_id: rand(1..31), value: [1,1,1, -1].sample )
-end
+  Vote.create(votable_type: "Question", votable_id: rand(183..213), voter_id: rand(234..264), value: [1,1,1, -1].sample )
+en
 
 300.times do
-  Vote.create(votable_type: "Answer", votable_id: rand(1..31), voter_id: rand(1..31), value: [1,1,1, -1].sample )
+  Vote.create(votable_type: "Answer", votable_id: rand(591..680), voter_id: rand(234..264), value: [1,1,1, -1].sample )
 end
